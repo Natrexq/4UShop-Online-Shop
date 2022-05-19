3478ca9ade05a058cfeb42ab1a5da42af88f09f2
Natanot <69844485+Natrexq@users.noreply.github.com>
2022-05-19 19:44:55 +0200

    Update index.php

--- bin/index.php
+++ bin/index.php
@@ -4,3 +4,25 @@ echo "test";
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+     <!---home index-->
+    <head>
+      
+        <title>4UShop.com</title>
+        
+    
+
+    </head>
+    <body>
+  
+    <h1 class="hl">Index.php</h1>
+    <div class="box" id="box">aa</div>
+
+    <link rel="stylesheet" href="css/main.css">
+
+
+    </body>
+
+</html>
