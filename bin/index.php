7b7bd3452fa205e5399012bc2e0b4fa9cab95319
Natanot <69844485+Natrexq@users.noreply.github.com>
2022-05-22 20:11:00 +0200

    Update index.php

--- bin/index.php
+++ bin/index.php
@@ -1,5 +1,5 @@
 <?php
-echo "test";
+
 
 
 
@@ -16,13 +16,38 @@ echo "test";
 
     </head>
     <body>
-  
-    <h1 class="hl">Index.php</h1>
-    <div class="box" id="box">aa</div>
-
     <link rel="stylesheet" href="css/main.css">
 
+        <div class="in_home">
+            <h1 class="xd">AAA</h1>
+
+        </div>
+        <div class="in_home_nav">
+            <h1 class="x">test</h1>
+
+        </div>
+        <div class="in_sh">
+
+
+
+
+        </div>
+        <div class="4in_end">
+            <div class="in_dev">
+                <h5 class="tx_dev">
+
+
+                </h5>
+
+            </div>
+        </div>
+
+
     
+
+
+
+
     </body>
 
 </html>
