4c73d490cd0ba7b74cf9768a3c8cabe92a4706ae
Natanot <69844485+Natrexq@users.noreply.github.com>
2022-05-23 18:01:17 +0200

    Update index.php

--- bin/index.php
+++ bin/index.php
@@ -20,6 +20,12 @@
     <link rel="stylesheet" href="css/main.css">
 
         <div class="in_home">
+            <div class="home_el">
+                <div class="el_lg">
+
+
+                </div>
+            </div>
             <h1 class="tx_home" id="foru">4USHOP</h1>
 
         </div>
@@ -31,25 +37,35 @@
             <h3 class="tx_home_nav">BUY</h3>
             <h3 class="tx_home_nav">ABOUT US</h3>
             <h3 class="tx_home_nav">DATA AND PRIVACY</h3>
-            <h3 class="tx_home_nav">SETTINGS</h3>
-
+        
         </div>
             
 
         </div>
         <div class="in_sh">
             <div class="sh_welcome">
-                <div class="wel_imgL"></div>
-                <div class="wel_imgR"></div>
+                <div class="wel_sld"></div>
+               
 
 
 
 
-            </div>
+             </div>
+         
 
 
 
         </div>
+        <div class="in_info">
+
+                <h1 class="tx_info">PRODUCTS</h1>
+
+        </div>
+        <div class="in_item">
+
+
+            
+    </div>
         <div class="in_end">
             <h1>End</h1>
             <div class="in_dev">
