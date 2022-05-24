37fa50876fd99057ade7084269f4244a728b646c
Natanot <69844485+Natrexq@users.noreply.github.com>
2022-05-24 18:00:33 +0200

    Update error.php

--- bin/error.php
+++ bin/error.php
@@ -9,12 +9,20 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Error: <?php echo $er_code; ?></title>
-
+    <title>Error: <?php echo $er_code;  ?></title>
+    <link rel="stylesheet" href="css/error.css">
 </head>
 
 <body>
     
+    <div class="er_backer">
+
+
+
+    </div>
+
+
+
 </body>
 
 
