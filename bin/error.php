32c7fc612162d88a472dc592645aa529c1f1431d
Natanot <69844485+Natrexq@users.noreply.github.com>
2022-05-24 17:49:29 +0200

    Create error.php

---
+++ bin/error.php
@@ -0,0 +1,22 @@
+<?php
+    $er_code = "404";
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Error: <?php echo $er_code; ?></title>
+
+</head>
+
+<body>
+    
+</body>
+
+
+
+</html>
