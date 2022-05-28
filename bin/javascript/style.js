563bc693b84dc168232d0c848d9cb8b4ea2b5fd6
Natanot <69844485+Natrexq@users.noreply.github.com>
2022-05-28 20:15:31 +0200

    Update style.js

--- bin/javascript/style.js
+++ bin/javascript/style.js
@@ -1,6 +1,14 @@
 // In this file you find every js function who appiles appearance of website
 
-function Slider(){
-
+function RgSlide(){
     
+    const image = document.getElementById('hmimg' +1);
+    alert('jd' + 1);
+
+       
+}
+function LfSlide(){
+    
+    
+       
 }
