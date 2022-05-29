88ab9024fe22d20832a9096dd700aafb6f8ec9ca
Natanot <69844485+Natrexq@users.noreply.github.com>
2022-05-29 22:06:55 +0200

    Update buy.js

--- bin/javascript/buy.js
+++ bin/javascript/buy.js
@@ -1,5 +1,28 @@
 // In this file you find buy product system
 
+function Buy(ida){
+            if(ida == "desp1"){
+                alert('ta');
+            }
+            else{
+                alert('no');
+            }
+
+
+
+        
+
+
+
+
+
+
+
+
+
+}
+
+
 function Slide(){
     const doc = document.getElementById('doel');
     doc.addEventListener('click', function DoClick(){
