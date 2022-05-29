e429227eee559410016d075781466cbaef33d1fa
Natanot <69844485+Natrexq@users.noreply.github.com>
2022-05-29 20:59:49 +0200

    Create buy.js

---
+++ bin/javascript/buy.js
@@ -0,0 +1,19 @@
+// In this file you find buy product system
+
+function Slide(){
+    const doc = document.getElementById('doel');
+    doc.addEventListener('click', function DoClick(){
+
+            alert('click1');
+
+    });
+    doc.addEventListener('drag', function DoDbClick(){
+
+        alert('dbclick!');
+
+    });
+
+    
+    
+
+}
