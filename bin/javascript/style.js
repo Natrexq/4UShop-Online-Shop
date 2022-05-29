fb892ce0d8eb81c295327de2b677e0ea1808110e
Natanot <69844485+Natrexq@users.noreply.github.com>
2022-05-29 19:39:14 +0200

    Update style.js

--- bin/javascript/style.js
+++ bin/javascript/style.js
@@ -1,24 +1,19 @@
 // In this file you find every js function who appiles appearance of website
 
-function RgSlide(){
+function Slide(){
+    const doc = document.getElementById('doel');
+    doc.addEventListener('click', function DoClick(){
+
+            alert('click1');
+
+    });
+    doc.addEventListener('drag', function DoDbClick(){
+
+        alert('dbclick!');
+
+    });
+
     
-    const back = document.getElementById('shback');
-    back.style.backgroundImage = "url('css/hmimg" +3 + ".jpg')"; 
-    alert(back.style.backgroundImage);
-
-       
-}
-function LfSlide(){
-
-    var one = 12;
-    fina = one + one;
-
-    alert(one);
-
-
-    const back = document.getElementById('shback');
-    back.style.backgroundImage = "url('css/hmimg" +1 + ".jpg')"; 
-    alert(back.style.backgroundImage);
     
-       
+
 }
