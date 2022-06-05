b5a206b0c16b36ef61e779e5c0394a3a33833a63
Natanot <69844485+Natrexq@users.noreply.github.com>
2022-06-05 21:02:29 +0200

    Update report.js

---
+++ bin/javascript/report.js
@@ -0,0 +1,48 @@
+
+function Report(a){
+
+    var global,inpval,message,svmessage,sendbut,wrdsts;
+
+        wrdsts = document.querySelector('.inptx_counter');
+
+        sendbut = document.getElementById('sndrepo');
+
+        inpval = document.getElementById('wrtrepo');
+
+        valinp = inpval.value;
+
+  
+
+
+    sendbut.addEventListener('click', function SndBut(){
+
+        if(valinp == ""){
+            alert("First you must write report");
+    
+
+
+        }
+        else{
+           alert(valinp);
+     
+
+        }
+
+
+
+         
+
+       
+
+        });
+    wrdsts.addEventListener('click', function WrdStats(){
+
+          alert("click");
+
+    });
+
+
+
+}
+
+
