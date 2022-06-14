cf9567997f7007ff344baa3a502fb68741ad906d
Natanot <69844485+Natrexq@users.noreply.github.com>
2022-06-14 19:28:16 +0200

    Update report.js

--- bin/javascript/report.js
+++ bin/javascript/report.js
@@ -1,3 +1,4 @@
+var empty = "";
 
 function FuCounter(){
 
@@ -16,14 +17,37 @@ function FuCounter(){
     counter.innerHTML = mathval + "/70";
     
     worldsinp.addEventListener('keydown', FuCounter, false);
+    worldsinp.addEventListener('blur', FuCounter, false);
 
-   
+   if(mathval == 70){
+    counter.innerHTML = "Max";
+   }
 
 }
 function Report(){
-    const forms = document.getElementById('forms');
-    alert(forms);
+    var worldsval = document.getElementById('wrtrepo').value;
+    var mathval = (0 + (worldsval.length));
+    
+    if(worldsval == empty){
+        alert("Warning: First you must write something in message box!");
+        return false;
+    }
+    if(mathval < 10){
+        alert("Warning: You must write more than 10 words!");
+    }
+    else{
+         alert("Error: your report: '"+worldsval+"' can't be send because report system has't be finished'");
+         return false;
+
+
+        
+
+    }
+    
+
 
 }
-window.addEventListener('load', Report, false);
+
+
+
 window.addEventListener('load', FuCounter, false);
