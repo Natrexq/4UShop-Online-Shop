d5db1d59d33f2a365c238d97f505606d7bd19fae
Natanot <69844485+Natrexq@users.noreply.github.com>
2022-06-16 23:51:07 +0200

    Update buy.js

--- bin/javascript/buy.js
+++ bin/javascript/buy.js
@@ -5,32 +5,48 @@ var er0x004 = "0x004: Unkown ";
 function aBuy(ida){
 
         const prod = ida;
-        var prod1,prod2,prod3;
+        var prod1,prod2,prod3,prod4,prod5,prod6,prod7,prod8,prod9;
         prod1 = "desp1";
         prod2 = "desp2";
         prod3 = "desp3";
+        prod4 = "desp1";
+        prod5 = "desp2";
+        prod6 = "desp3";
+        prod7 = "desp1";
+        prod8 = "desp2";
+        prod9 = "desp3";
 
 
             if(prod == prod1){
-                //PHP SENDS INFO
-                window.open('shopping.php');
+            
+                alert("1");
+                return false;
+            }
+            if(prod == prod2){
+            
+                alert("2");
+                return false;
+            }
+            if(prod == prod3){
+            
+                alert("3");
+                return false;
+            }
+            if(prod == prod4){
+            
+                alert("4");
+                return false;
+            }
+            if(prod == prod5){
+            
+                alert("5");
+                return false;
             }
             else{
-                if(prod == prod2){
-                     //PHP SENDS INFO
-                    window.open('shopping.php');
-                }
-                else{
-                    if(prod == prod3){
-                         //PHP SENDS INFO
-                        window.open('shopping.php');
-
-                    }
-                    else{
-                        console.log(er0x000);
-                    }
-                }
+                  console.log(er0x000);
             }
+                
+            
 
 
 
