d6159d23aaafe93f2f8d5f62e5776067289b08ad
Natanot <69844485+Natrexq@users.noreply.github.com>
2022-06-17 00:23:52 +0200

    Update index.php

--- bin/index.php
+++ bin/index.php
@@ -121,7 +121,7 @@
                
 
                            
-                            <div class="sl_pro" id="pr1"><div class="pr_img" id="impr1"></div><h2 class="pr_desc" id="desp1" >Gaming PC: GTX1650 SUPER ,I5 10TH,16GB RAM,500GB HD</h2></div>
+                            <div class="sl_pro" id="pr1"><div class="pr_img" id="impr1"></div><h2 name="asda" class="pr_desc" id="desp1" >Gaming PC: GTX 1650 SUPER ,I5 10TH,16GB RAM,500GB HD</h2></div>
                             <div class="sl_pro" id="pr2"><div class="pr_img" id="impr2"></div><h2 class="pr_desc" id="desp2" >Unkown</h2></div>
                             <div class="sl_pro" id="pr3"><div class="pr_img" id="impr3"></div><h2 class="pr_desc" id="desp3" >-</h2></div>
                        
