b8c13e8099e9f946b27d0c0c7e6dc09e2b83cdf8
Natanot <69844485+Natrexq@users.noreply.github.com>
2022-06-21 22:52:48 +0200

    Update connect.php

---
+++ bin/connect.php
@@ -0,0 +1,13 @@
+<?php
+    //connect with database
+
+
+    $host = "localhost"; //host data
+    $user = "root"; //login to database for example root,admin
+    $pass = "root"; //password to database
+    $database_name = "4ushopdb"; //name of the database who you want to connect
+
+
+
+
+?>
