c66db29950ac9fe29c89c0f1fb632dd7dd6d9a34
Natanot <69844485+Natrexq@users.noreply.github.com>
2022-07-03 15:41:59 +0200

    Create search.js

---
+++ bin/javascript/search.js
@@ -0,0 +1,23 @@
+function Search(){
+    // it works !alert("Test comunicate!");
+
+    const srchval = document.getElementById('');
+    const srchbut = document.getElementsById('');
+    
+  srchbut.addEventListener('click', DoSearch,false);
+
+
+        function DoSearch(){
+
+            if(srchval == "computer"){
+
+                location.href = "index.php";
+
+
+            }
+
+        }
+
+
+}
+window.addEventListener('load', Search,false);
