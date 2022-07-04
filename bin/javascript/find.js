ba8565285113418180fe36c648e92106b06f3cc4
Natanot <69844485+Natrexq@users.noreply.github.com>
2022-07-04 20:21:26 +0200

    Update find.js

--- bin/javascript/find.js
+++ bin/javascript/find.js
@@ -1,6 +1,6 @@
 function Search(){
 
-    var srbut,srinp,srval,website,scroll,slist1,slist2,inpage,index1,index2;
+    var srbut,srinp,srval,website,scroll,slist1,slist2,slist3,index1,index2,index3,easteregg,inegg;
 
     srbut = document.getElementById('srbut');
     srinp = document.getElementById('srinp');
@@ -21,13 +21,33 @@ function Search(){
                     
                     slist1 = ["shop", "offer", "buy", "pc", "computer", "gameing", "game", "games", "all in one", "product", "products", "cpu", "gpu"];
                     index1 = slist1.includes(srval);
-
+                    //
+                    slist2 = ["privacy", "rules", "rule", "term", "terms", "licence", "cookie", "cookies", "users", "settings", "pv", "data", "regulations", "user data", "regulations"];
+                    index2 = slist2.includes(srval);
+                    //
+                    slist3 = ["help", "problem", "problems", "support", "bug", "bugs", "report", "advice", "service"];
+                    index3 = slist3.includes(srval);
+                    //
+                    easteregg = ["69", "420", "ass", "fuck", "blayt", "poo"];
+                    inegg = easteregg.includes(srval);
 
                     if(website == "/index.php"){
                         if(index1 == true){
                         
                             scroll.scrollIntoView();
                            
+                        }
+                        if(index2 == true){
+                            alert("If you wanna find: rules,terms,cookies and the like, go to the rules section");
+                        }
+                        if(index3 == true){
+                            alert("If you need help,our support or report bugs, go to the help section");
+                        }
+                        if(inegg == true){
+                            alert("Ok?");
+                            window.close();
+                            window.open("https://www.youtube.com/watch?v=QDia3e12czc");
+                            
                         }
                         else{
                             alert("I can't find it on website :/ , change the content of text input or try again");
@@ -36,8 +56,11 @@ function Search(){
                     else{
                         
                         if(index1 == true){
-                            alert("If you wanna find shop offer, please click home button and scroll down");
+                            alert("If you wanna find shop offer, go to the home section and scroll down");
+                            return false;
                         }
+                    
+
                         else{
                             alert("I can't find it on website :/ , change the content of text input or try again");
                         }
